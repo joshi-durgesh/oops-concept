8a77d8239a27749b5251ae6eccc40da9aa3defb8
joshi-durgesh <durgesh7joshi9@gmail.com>
2023-12-26 00:54:56 +0530

    using and connecting multiple classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,8 +12,31 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A pillow",
       "https://www.gooten.com/wp-content/uploads/2021/04/Throw_Pillow_Catalog_Photo_01.png",
@@ -26,30 +49,22 @@ const productList = {
       "A carpet which you might like - or not.",
       34.53
     ),
-  ],
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}">
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
